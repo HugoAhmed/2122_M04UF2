31876a348a3fccb0b0f658074a4e1cc9b8c07e77
enti <enti@enti.enti>
2022-03-31 21:50:45 +0200

    Deberes hechos

--- todolist/src/RemoveTask.js
+++ todolist/src/RemoveTask.js
@@ -1,16 +1,69 @@
+import react from 'react';
 import Button from '@mui/material/Button';
-
+import Dialog from '@mui/material/Dialog'
 import DeleteIcon from '@mui/icons-material/Delete';
+import DialogAction from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 
-function RemoveTask (props)
+class RemoveTask extends react.Component
 {
-	return (
-		<Button startIcon={<DeleteIcon />} onClick={function (event){
-				props.removeTask(props.id_task);
-			}
-		}>BORRAR</Button>
-	);
+	constructor (props){
+		super(props);
+		this.state = 
+		{ 
+						open: false};
+		}
+	handleOnClick = ()=>
+	{
+	this.setState({open: true });
+	}
+	handleClose = () => 
+	{
+	this.setState({open:false});
+	}
+	handleDelete = () =>
+	{
+	this.props.removeTask(this.props.task_id);
+	this.setState({open:false});
+	}
+
+
+ render() {
+ 	return (
+	<div>
+		<Button variant="contained" 
+				startIcon=<DeleteIcon /> 
+				onClick={this.handleOnClick}>	
+
+		Eliminar
+		 
+		</Button>
+
+		<Dialog  
+			open={this.state.open}
+			onClose={this.handleClose}
+			>
+			<DialogTitle>
+				{'Estas seguro?'}
+					</DialogTitle>
+			<DialogAction>
+				<Button onClick={this.handleDelete}>
+					confirmar
+						</Button>
+				<Button onClick={this.handleClose}> 
+
+					Cancelar
+					</Button>
+
+			</DialogAction>
+		</Dialog>
+</div>
+
+
+
+);
 
 }
-
+}
 export default RemoveTask;
--- todolist/src/TaskSubmit.js
+++ todolist/src/TaskSubmit.js
@@ -1,9 +1,34 @@
-function TaskSubmit ()
-{
-	return (
-		<button>AÃ±adir</button>
+import react from 'react';
+import AddCircle from '@mui/icons-material/AddCircle';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+
+
+
+class  TaskSubmit extends react.Component{
+	constructor (props){
+	
+		super(props);
+
+	}
+handleSubmit = (event) => {
+	event.preventDefault();
+	this.props.onSubmit(event);
+
+}
+
+render(){
+	return(
+	
+	<Tooltip title="Añadir" >
+		<Button variant="contained" onClick={this.handleSubmit}>
+			<AddCircle/>
+				</Button>
+	</Tooltip>
 	);
 
+
+}
 }
 
 export default TaskSubmit;
